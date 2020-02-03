52e92836a55f7170f253780523254bd06d042e63
gothaw <rad.soltan@gmail.com>
2020-02-03 22:42:21 +0000

    Finished getting started.

--- beginning-java/java-basics/getting-started/src/com/gettingstarted/Main.java
+++ beginning-java/java-basics/getting-started/src/com/gettingstarted/Main.java
@@ -1,8 +1,28 @@
 package com.gettingstarted;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        /*
+            Workspace uses:  Console console = System.console();
+            Which does not work in IDE
+         */
+        System.out.println("Hello world");
+        System.out.println("This is new line.\nThis is new line.\n");
+
+        // scanner
+        System.out.println("What is your name???");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Hello " + firstName);
+        // Format or printf can be used with parameters
+        System.out.printf("%s is learning Java", firstName);
+
+
     }
 }
