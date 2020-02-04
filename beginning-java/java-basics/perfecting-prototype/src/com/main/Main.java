bbdcb6bb3ad0eef7472d08d29e5b8626d7afd2d5
gothaw <rad.soltan@gmail.com>
2020-02-04 23:56:58 +0000

    Finished perfecting prototype class.

---
+++ beginning-java/java-basics/perfecting-prototype/src/com/main/Main.java
@@ -0,0 +1,58 @@
+package com.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your age?");
+
+        // AGE VALIDATION
+
+        String ageAsString = scanner.nextLine();
+        int age = Integer.parseInt(ageAsString);
+        // Other way:
+        // int age = scanner.nextInt();
+        // scanner.nextLine();
+        if (age < 13) {
+            // Insert exit code
+            System.out.println("Sorry you must be at least 13 to use this program.");
+            // Program terminates 0 - program finished intentionally any other parameter means that program finish abnormally
+            System.exit(0);
+        }
+
+        // __Name__ is a __adjective__ __noun__. They are always __adverb__ __verb__.
+
+        System.out.println("Enter a name:  ");
+        String name = scanner.nextLine();
+        System.out.println("Enter an adjective:  ");
+        String adjective = scanner.nextLine();
+
+        // NOUN VALIDATION
+        System.out.println("Enter a noun:  ");
+        String noun;
+        String[] forbiddenNamesArray = {"dork"};
+        boolean isInvalidWord;
+
+        do {
+            noun = scanner.nextLine();
+
+            isInvalidWord = noun.equalsIgnoreCase("dork") || noun.equalsIgnoreCase("jerk");
+
+            if (isInvalidWord) {
+                System.out.println("This language is not allowed!");
+            }
+        } while (isInvalidWord);
+
+        System.out.println("Enter an adverb:  ");
+        String adverb = scanner.nextLine();
+        System.out.println("Enter a verb ending with -ing:  ");
+        String verb = scanner.nextLine();
+
+        System.out.println("Your tree story: \n ------------- \n");
+        System.out.printf("%s is a %s %s. ", name, adjective, noun);
+        System.out.printf("They are always %s %s. \n", adverb, verb);
+    }
+}
