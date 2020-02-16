ef603368a5ea7c8ae8d4d0f0fb5e838c2f13cd84
gothaw <rad.soltan@gmail.com>
2020-02-16 12:10:35 +0000

    Finished class - deliver MVP.

--- beginning-java/java-objects/deliver-mvp/src/com/main/Game.java
+++ beginning-java/java-objects/deliver-mvp/src/com/main/Game.java
@@ -7,18 +7,45 @@ public class Game {
     private String misses;
 
     public Game(String answer) {
-        this.answer = answer;
+        this.answer = answer.toLowerCase();
         hits = "";
         misses = "";
     }
 
+    public String getAnswer() {
+        return answer;
+    }
+
+    private char normalizedGuess(char letter) {
+        if (!Character.isLetter(letter)) {
+            throw new IllegalArgumentException("A letter is required.");
+        }
+        letter = Character.toLowerCase(letter);
+        if (hits.indexOf(letter) != -1 || misses.indexOf(letter) != -1) {
+            throw new IllegalArgumentException(letter + " has already been guessed.");
+        }
+        return letter;
+    }
+
+    public void applyGuess(String letters) {
+        if (letters.length() == 0) {
+            throw new IllegalArgumentException("No letter found.");
+        }
+        applyGuess(letters.charAt(0));
+    }
+
     public void applyGuess(char letter) {
+        letter = normalizedGuess(letter);
         boolean isHit = answer.indexOf(letter) != -1;
         if (isHit) {
             hits += letter;
         } else {
             misses += letter;
         }
+    }
+
+    public boolean isWon() {
+        return getCurrentProgress().indexOf('-') == -1;
     }
 
     public int getRemainingTries() {
--- beginning-java/java-objects/deliver-mvp/src/com/main/Prompter.java
+++ beginning-java/java-objects/deliver-mvp/src/com/main/Prompter.java
@@ -11,14 +11,29 @@ public class Prompter {
 
     public void promptForGuess() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a later:   ");
-        String guessInput = scanner.nextLine();
-        char guess = guessInput.charAt(0);
-        game.applyGuess(guess);
+        boolean isAcceptable = false;
+        do{
+            System.out.print("Enter a later:   ");
+            String guessInput = scanner.nextLine();
+            try{
+                game.applyGuess(guessInput);
+                isAcceptable = true;
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        } while (!isAcceptable);
     }
 
     public void displayProgress(){
         System.out.printf("You have %d tries left to solve: %s%n", game.getRemainingTries(), game.getCurrentProgress());
+    }
+
+    public void displayOutcome(){
+        if(game.isWon()){
+            System.out.printf("Congratulation you have won with %d tries left. %n", game.getRemainingTries());
+        } else{
+            System.out.printf("Bummer. You've lost. The word was: %s %n.", game.getAnswer());
+        }
     }
 
 }
