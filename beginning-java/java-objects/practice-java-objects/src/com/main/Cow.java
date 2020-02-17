5b283761bf1952bc16d4496db326713a27f4b241
gothaw <rad.soltan@gmail.com>
2020-02-17 00:13:21 +0000

    Extra lesson - Practice Java Objects.

---
+++ beginning-java/java-objects/practice-java-objects/src/com/main/Cow.java
@@ -0,0 +1,13 @@
+package com.main;
+
+public class Cow {
+    private final String name;
+
+    public Cow(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ beginning-java/java-objects/practice-java-objects/src/com/main/Main.java
@@ -0,0 +1,10 @@
+package com.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cow cow = new Cow("Larry");
+
+        System.out.println("Cow name is " + cow.getName());
+    }
+}
