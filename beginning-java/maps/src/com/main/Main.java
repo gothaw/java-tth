01a84a6fe2a219ef261061beed3e137442c9c2c2
gothaw <rad.soltan@gmail.com>
2020-03-01 21:44:54 +0000

    Finished Java Maps Class.

--- beginning-java/maps/src/com/main/Main.java
+++ beginning-java/maps/src/com/main/Main.java
@@ -1,8 +1,42 @@
 package com.main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
+    public static final String BREAKFAST = "breakfast";
+    public static final String LUNCH = "lunch";
+    public static final String DINNER = "dinner";
+
     public static void main(String[] args) {
-	// write your code here
+        Map<String, String> meals = new HashMap<>();
+
+        // adding values
+        meals.put(BREAKFAST,"waffles");
+        meals.put(LUNCH,"gyros");
+        meals.put(DINNER,"enchiladas");
+
+        System.out.println(meals);
+
+        // getting values
+        System.out.println(meals.get(DINNER));
+
+        // removing values
+        String lunch = meals.remove(LUNCH);
+        System.out.println(lunch);
+
+        // checking if contains a key or value
+        meals.put(LUNCH,"chicken");
+
+        boolean hasLunch = meals.containsKey(LUNCH);
+        boolean hasBrunch = meals.containsValue("waffles");
+        System.out.println(hasLunch);
+        System.out.println(hasBrunch);
+
+        // size
+        int size = meals.size();
+        System.out.println(size);
+
     }
 }
