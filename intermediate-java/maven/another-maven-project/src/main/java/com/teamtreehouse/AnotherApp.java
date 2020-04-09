a9da1c69496a2eae367e794b3612fa0760cc23da
gothaw <rad.soltan@gmail.com>
2020-04-09 17:18:07 +0100

    Finished class - Intro to Maven.

---
+++ intermediate-java/maven/another-maven-project/src/main/java/com/teamtreehouse/AnotherApp.java
@@ -0,0 +1,7 @@
+package com.teamtreehouse;
+
+public class AnotherApp {
+    public static void main(String[] args) throws Exception {
+        App.main(args);
+    }
+}
---
+++ intermediate-java/maven/file-spy/src/main/java/com/teamtreehouse/App.java
@@ -0,0 +1,38 @@
+package com.teamtreehouse;
+
+import java.nio.file.*;
+import org.apache.tika.Tika;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static final String FILE_TYPE = "text/csv";
+    public static final String DIR_TO_WATCH = "E:/Documents/TTH/java";
+
+    public static void main( String[] args ) throws Exception
+    {
+        Path dir = Paths.get(DIR_TO_WATCH);
+        Tika tika = new Tika();
+        WatchService watchService = FileSystems.getDefault().newWatchService();
+        dir.register(watchService, ENTRY_CREATE);
+        WatchKey key;
+        do{
+            key = watchService.take();
+
+            key.pollEvents().stream().filter( e -> {
+                Path filename = (Path)e.context();
+                String type = tika.detect(filename.toString());
+                return FILE_TYPE.equals(type);
+            }).forEach(e ->
+                    System.out.printf("File found: %s%n", e.context())
+            );
+
+
+        } while (key.reset());
+    }
+}
