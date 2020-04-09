8149bdecc60ebd685a03574868304b7049a0a689
gothaw <rad.soltan@gmail.com>
2020-04-09 14:02:49 +0100

    Finished class - custom annotations.

--- intermediate-java/annotations/custom-annotations/src/main/java/com/teamtreehouse/docgen/Main.java
+++ intermediate-java/annotations/custom-annotations/src/main/java/com/teamtreehouse/docgen/Main.java
@@ -2,28 +2,8 @@ package com.teamtreehouse.docgen;
 
 import com.teamtreehouse.math.MathUtils;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-
 public class Main {
     public static void main(String[] args) {
-        // Getting a class object
-        Class<?> clazz = MathUtils.class;
-
-        // Get all declared methods
-        Method[] methods = clazz.getDeclaredMethods();
-
-        for (Method method : methods) {
-            // Display method name
-            System.out.printf("name: %s%n", method.getName());
-            // Display parameter count
-            System.out.printf("\t# params: %s%n", method.getParameterCount());
-            // Display return type
-            System.out.printf("\treturn type: %s%n", method.getReturnType().getSimpleName());
-            // Display modifiers
-            int mods = method.getModifiers();
-            String modStr = Modifier.toString(mods);
-            System.out.printf("\tmodifiers: %s%n", modStr);
-        }
+        DocProcessor.process(MathUtils.class);
     }
 }
--- intermediate-java/annotations/custom-annotations/src/main/java/com/teamtreehouse/math/MathUtils.java
+++ intermediate-java/annotations/custom-annotations/src/main/java/com/teamtreehouse/math/MathUtils.java
@@ -1,6 +1,7 @@
 package com.teamtreehouse.math;
 
 import java.awt.geom.Point2D;
+
 import com.teamtreehouse.docgen.Doc;
 
 @Doc(
@@ -9,25 +10,36 @@ import com.teamtreehouse.docgen.Doc;
 public class MathUtils {
     private static final Double EPSILON = 0.0001;
 
+
     @Doc(
             desc = "Calculates the are of the triangle",
-            params = {"Coordinates of the first vertex"},
+            params = {"Coordinates of the first vertex", "Second vertex", "Third vertex"},
             returnVal = "Calculated are of the triangle"
     )
     public static Double triangleArea(Point2D.Double a, Point2D.Double b, Point2D.Double c) {
         return 0.0;
     }
 
+    /**
+     * Calculates distance between two points
+     * @param a - point one
+     * @param b - point two
+     * @return distance
+     */
     @Doc(
             desc = "Calculates the distance between the given points",
             params = {"Coordinate of one point", "Coordinate of another point"},
-            returnVal = ""
+            returnVal = "Distance between two points"
     )
     public static Double distance(Point2D.Double a, Point2D.Double b) {
         return 0.0;
     }
 
-
+    @Doc(
+            desc = "Calculates quadratic root for three numbers",
+            params = {"First number", "Second number", "Third number"},
+            returnVal = "An array"
+    )
     public static Double[] quadraticRoots(int a, int b, int c) {
         return new Double[]{};
     }
