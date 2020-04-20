1fb37ba8e16481eff205313e5651697d5ec0a634
gothaw <rad.soltan@gmail.com>
2020-04-20 11:44:21 +0100

    Added ability to create fat jar using maven - modular app.

---
+++ intermediate-java/java-fx/maven-modular/src/main/java/com/teamtreehouse/pomodoro/EntryPoint.java
@@ -0,0 +1,9 @@
+package com.teamtreehouse.pomodoro;
+
+import javafx.application.Application;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        Application.launch(App.class);
+    }
+}
