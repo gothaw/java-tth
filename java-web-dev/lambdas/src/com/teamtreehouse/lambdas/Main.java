f5fc5d348b829f1cfe011f64de5fd12c41849696
gothaw <rad.soltan@gmail.com>
2023-03-26 12:43:06 +0100

    Updating to project files from downloads.

--- java-web-dev/lambdas/src/com/teamtreehouse/lambdas/Main.java
+++ java-web-dev/lambdas/src/com/teamtreehouse/lambdas/Main.java
@@ -1,5 +1,11 @@
+package com.teamtreehouse.lambdas;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+	// write your code here
+
     }
 }
