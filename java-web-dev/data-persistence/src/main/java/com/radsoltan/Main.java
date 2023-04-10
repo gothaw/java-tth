23e827bdd915ee2e49526e56265dffaa0453f989
gothaw <rad.soltan@gmail.com>
2023-04-10 16:39:46 +0100

    Adding save method

--- java-web-dev/data-persistence/src/main/java/com/radsoltan/Main.java
+++ java-web-dev/data-persistence/src/main/java/com/radsoltan/Main.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO: Load the SQLite JDBC driver (JDBC class implements java.sql.Driver)
+        // Load the SQLite JDBC driver (JDBC class implements java.sql.Driver)
         try {
             // Registering driver method
             Class.forName("org.sqlite.JDBC");
@@ -19,24 +19,26 @@ public class Main {
             throw new RuntimeException(e);
         }
 
-        // TODO: Create a DB connection
+        // Create a DB connection
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:contactmgr.db")) {
             System.out.println("Connection established");
-            // TODO: Create a SQL statement
+            // Create a SQL statement
             Statement statement = connection.createStatement();
 
-            // TODO: Create a DB table
+            // Create a DB table
             statement.executeUpdate("DROP TABLE IF EXISTS contacts");
             statement.executeUpdate("CREATE TABLE contacts (id INTEGER PRIMARY KEY, firstname STRING, lastname STRING, email STRING, phone INT(10))");
 
-            // TODO: Insert a couple contacts
-            statement.executeUpdate("INSERT INTO contacts (firstname, lastname, email, phone) VALUES('Chris', 'Rama', 'rama@mail.com', 1234567890)");
-            statement.executeUpdate("INSERT INTO contacts (firstname, lastname, email, phone) VALUES('James', 'Blinq', 'blinq@mail.com', 1234567890)");
+            // Insert a couple contacts
+            Contact c = new Contact("Chris", "Rama", "rama@mail.com", 1234567890L);
+            save(c, statement);
+            c = new Contact("James", "Blinq", "blinq@mail.com", 1234567890L);
+            save(c, statement);
 
-            // TODO: Fetch all the records from the contacts table
+            // Fetch all the records from the contacts table
             ResultSet rs = statement.executeQuery("SELECT * FROM contacts");
 
-            // TODO: Iterate over the ResultSet & display contact info
+            // Iterate over the ResultSet & display contact info
             while (rs.next()) {
                 int id = rs.getInt("id");
                 String firstName = rs.getString("firstname");
@@ -49,5 +51,13 @@ public class Main {
             // Display connection or query errors
             System.err.printf("There was a database error: %s%n",ex.getMessage());
         }
+    }
+
+    public static void save(Contact contact, Statement statement) throws SQLException {
+        // Compose query
+        String sql = "INSERT INTO contacts (firstname, lastname, email, phone) VALUES ('%s','%s','%s',%d)";
+        sql = String.format(sql, contact.getFirstName(), contact.getLastName(), contact.getEmail(), contact.getPhone());
+        // Execute query
+        statement.executeUpdate(sql);
     }
 }
